43b16c3b494a4fe78df15399cbaa640bfe795f2f
SardineFish <Sardine@live.cn>
2017-10-03 18:27:48 +0800

    make camera shake!

---
+++ Monument Valley/Assets/Camera/ShakeCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour {
+    public float Range = 1;
+    public float Strength = 1f;
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Mathf.PerlinNoise(Time.time * Strength, 1);
+        var y = Mathf.PerlinNoise(1, Time.time * Strength);
+        x -= 0.5f;
+        y -= 0.5f;
+        x *= Range;
+        y *= Range;
+        transform.localPosition = new Vector3(x, y, 0);
+	}
+}
