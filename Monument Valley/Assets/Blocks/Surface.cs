7b37ab9dacf98210d227f9864e41a55e5887c815
SardineFish <Sardine@live.cn>
2017-10-04 14:35:00 +0800

    Designed Map-2

--- Monument Valley/Assets/Blocks/Surface.cs
+++ Monument Valley/Assets/Blocks/Surface.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class Surface : MonoBehaviour {
+public class Surface : MonoBehaviour
+{
     public GameObject Waypoint;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         var waypoint = this.transform.Find("Waypoint");
         if (waypoint != null)
             Waypoint = waypoint.gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnMouseUp()
     {
