5d3b6cbf32bf8181590d404663536ce30c9473d0
SardineFish <Sardine@live.cn>
2017-10-05 22:17:58 +0800

    Bug fixed.

---
+++ Monument Valley/Assets/Map-3/Map3GameSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map3GameSystem : GameSystem
+{
+
+    public override void GameEndingCallback()
+    {
+        NextMap();
+        GameObject.Find("Map-3").SetActive(false);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
